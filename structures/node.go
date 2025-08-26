c00122a006748e794256a2ae1034910470c8124c
jigyansunanda <nandajigyansu@gmail.com>
2025-08-26 09:56:09 +0530

    Add Node structure and NewNode function for typeahead implementation
    Signed-off-by: jigyansunanda <nandajigyansu@gmail.com>

---
+++ structures/node.go
@@ -0,0 +1,17 @@
+package structures
+
+type Node struct {
+	Child       map[string]Node
+	Frequency   int32
+	IsEndOfWord bool
+}
+
+func NewNode() *Node {
+	node := &Node{
+		Child:       nil,
+		Frequency:   0,
+		IsEndOfWord: false,
+	}
+
+	return node
+}
