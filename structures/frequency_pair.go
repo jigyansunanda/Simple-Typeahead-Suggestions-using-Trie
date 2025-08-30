02b70c7e292262bd54ac965a81343941028531a1
jigyansunanda <nandajigyansu@gmail.com>
2025-08-30 09:59:10 +0530

    Export FrequencyPair struct fields
    Signed-off-by: jigyansunanda <nandajigyansu@gmail.com>

--- structures/frequency_pair.go
+++ structures/frequency_pair.go
@@ -1,9 +1,9 @@
 package structures
 
 type FrequencyPair struct {
-	index     int
-	word      string
-	frequency int32
+	Index     int
+	Word      string
+	Frequency int32
 }
 
 type MyHeap []*FrequencyPair
@@ -11,21 +11,21 @@ type MyHeap []*FrequencyPair
 func (pq MyHeap) Len() int { return len(pq) }
 
 func (pq MyHeap) Less(i, j int) bool {
-	if pq[i].frequency != pq[j].frequency {
-		return pq[i].frequency > pq[j].frequency
+	if pq[i].Frequency != pq[j].Frequency {
+		return pq[i].Frequency > pq[j].Frequency
 	} else {
-		return pq[i].word < pq[j].word
+		return pq[i].Word < pq[j].Word
 	}
 }
 
 func (pq MyHeap) Swap(i, j int) {
 	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index, pq[j].index = i, j
+	pq[i].Index, pq[j].Index = i, j
 }
 
 func (pq *MyHeap) Push(x any) {
 	frequencyPair := x.(*FrequencyPair)
-	frequencyPair.index = len(*pq)
+	frequencyPair.Index = len(*pq)
 	*pq = append(*pq, frequencyPair)
 }
 
