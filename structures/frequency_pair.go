94bb0d276232fa5f014f3fe2902df07cfd61812c
jigyansunanda <nandajigyansu@gmail.com>
2025-08-30 09:53:42 +0530

    Sort frequency pairs by word if frequencies match
    Signed-off-by: jigyansunanda <nandajigyansu@gmail.com>

--- structures/frequency_pair.go
+++ structures/frequency_pair.go
@@ -8,8 +8,16 @@ type FrequencyPair struct {
 
 type MyHeap []*FrequencyPair
 
-func (pq MyHeap) Len() int           { return len(pq) }
-func (pq MyHeap) Less(i, j int) bool { return pq[i].frequency > pq[j].frequency }
+func (pq MyHeap) Len() int { return len(pq) }
+
+func (pq MyHeap) Less(i, j int) bool {
+	if pq[i].frequency != pq[j].frequency {
+		return pq[i].frequency > pq[j].frequency
+	} else {
+		return pq[i].word < pq[j].word
+	}
+}
+
 func (pq MyHeap) Swap(i, j int) {
 	pq[i], pq[j] = pq[j], pq[i]
 	pq[i].index, pq[j].index = i, j
