6971bfda97a1e9099f30f635f99eef2eb1456297
jigyansunanda <nandajigyansu@gmail.com>
2025-08-30 10:10:35 +0530

    Add InsertWord method to Trie data structure
    Signed-off-by: jigyansunanda <nandajigyansu@gmail.com>

--- structures/trie.go
+++ structures/trie.go
@@ -9,3 +9,20 @@ func NewTrie() *Trie {
 
 	return trie
 }
+
+func (trie *Trie) InsertWord(word string) {
+	currentNode := trie.Root
+	for _, char := range word {
+		// Convert rune to string
+		str := string(char)
+		// Check if node exists or not
+		childNode, isExist := currentNode.Child[str]
+		if !isExist {
+			currentNode.Child[str] = NewNode()
+			childNode = currentNode.Child[str]
+		}
+		currentNode = childNode
+	}
+	currentNode.IsEndOfWord = true
+	currentNode.Frequency += 1
+}
