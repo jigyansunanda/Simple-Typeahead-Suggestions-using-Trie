0989a93bf061b388c8e500a4db8d623ff65f7ee8
jigyansunanda <nandajigyansu@gmail.com>
2025-08-30 11:20:44 +0530

    Add dictionary suggestion lookup with min heap
    Signed-off-by: jigyansunanda <nandajigyansu@gmail.com>

--- services/dictionary.go
+++ services/dictionary.go
@@ -1,6 +1,12 @@
 package services
 
-import "github.com/jigyansunanda/Simple-Typeahead-Suggestions-using-Trie/structures"
+import (
+	"container/heap"
+
+	"github.com/jigyansunanda/Simple-Typeahead-Suggestions-using-Trie/structures"
+)
+
+const MAX_SUGGESTION_COUNT = 5
 
 type Dictionary struct {
 	trie          structures.Trie
@@ -9,8 +15,7 @@ type Dictionary struct {
 
 func NewDictionary() Dictionary {
 	dictionary := &Dictionary{
-		trie:          *structures.NewTrie(),
-		priorityQueue: structures.MyHeap{},
+		trie: *structures.NewTrie(),
 	}
 
 	return *dictionary
@@ -22,4 +27,39 @@ func (dictionary *Dictionary) InsertWordIntoDictionary(word string) {
 
 func (dictionary *Dictionary) SearchPrefixInDictionary(word string) bool {
 	return dictionary.trie.SearchPrefix(word)
+}
+
+func (dictionary *Dictionary) getSuggestions(prefix string, myHeap *structures.MyHeap) {
+	currentNode := dictionary.trie.Root
+	for _, char := range prefix {
+		str := string(char)
+		currentNode = currentNode.Child[str]
+	}
+	for str := range currentNode.Child {
+		getSuggestionsUtil(currentNode.Child[str], prefix+str, myHeap)
+	}
+}
+
+func getSuggestionsUtil(node *structures.Node, word string, myHeap *structures.MyHeap) {
+	if node == nil {
+		return
+	}
+	if node.IsEndOfWord {
+		if myHeap.Len() < MAX_SUGGESTION_COUNT {
+			heap.Push(myHeap, &structures.FrequencyPair{Word: word, Frequency: node.Frequency})
+		} else {
+			if myHeap.Peek().(*structures.FrequencyPair).Frequency < node.Frequency {
+				heap.Pop(myHeap)
+				heap.Push(myHeap, &structures.FrequencyPair{Word: word, Frequency: node.Frequency})
+			} else if myHeap.Peek().(*structures.FrequencyPair).Frequency == node.Frequency {
+				if myHeap.Peek().(*structures.FrequencyPair).Word < word {
+					heap.Pop(myHeap)
+					heap.Push(myHeap, &structures.FrequencyPair{Word: word, Frequency: node.Frequency})
+				}
+			}
+		}
+	}
+	for str := range node.Child {
+		getSuggestionsUtil(node.Child[str], word+str, myHeap)
+	}
 }
